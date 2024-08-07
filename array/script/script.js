6bce8441e4deaf85dc4fb3e1c06284907256f22d
Mobina <Mobinaabdollahi20@gmail.com>
2024-08-07 13:46:51 +0330

    Add files via upload

---
+++ array/script/script.js
@@ -0,0 +1,13 @@
+const sport=["Teakwondo","Swim","Skate","Volleyball","Gymnastics"];
+console.log(sport)
+sport.unshift("I love");
+console.log(sport);
+const between=sport.join(" and ");
+console.log(between)
+sport.pop();
+console.log(sport);
+const sport2=["Basketball","Badminton","Water polo","Fitness","Yoga"];
+console.log(sport2)
+sport2.splice(0,2);
+console.log(sport2)
+
