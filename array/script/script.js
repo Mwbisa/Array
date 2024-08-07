8cd0861eaaa22b61e02470a5df7282ebabbd9f5a
Mobina <Mobinaabdollahi20@gmail.com>
2024-08-07 14:37:00 +0330

    Add files via upload

--- array/script/script.js
+++ array/script/script.js
@@ -10,4 +10,7 @@ const sport2=["Basketball","Badminton","Water polo","Fitness","Yoga"];
 console.log(sport2)
 sport2.splice(0,2);
 console.log(sport2)
-
+const index0=sport2[0];
+console.log(index0)
+const indexes=sport2.slice(1,3)
+console.log(indexes.toString());
